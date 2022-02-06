4b367dc6f8cb3be19197f6f0d6d0eafeb6a35404
Kevin Kristoffer Oporto <kevin042@Kevins-MacBook-Pro.local>
2022-02-06 19:09:49 +0800

    Docker Configuration

---
+++ plugins/monster-grp/monster-grp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package MonsterGroup
+ */
+/*
+PLUGIN NAME: Monster Group Leads
+DESCRIPTION: Code task for Monster Group application
+VERSION: 1.0.0
+AUTHOR: Kevin Kristoffer Oporto
+*/
